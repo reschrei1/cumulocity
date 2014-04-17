02fe640c835704c297756ea5840d7d87e5ff312a
Dominik Bielicki <dominik.bielicki@impaqgroup.com>
2014-04-17 18:38:06 +0200

    merge two heads

---
+++ tracker-agent/src/main/java/c8y/trackeragent/devicebootstrap/DeviceBootstrapProcessor.java
@@ -0,0 +1,104 @@
+package c8y.trackeragent.devicebootstrap;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import c8y.trackeragent.TrackerAgent;
+import c8y.trackeragent.event.TrackerAgentEventListener;
+import c8y.trackeragent.event.TrackerAgentEvents;
+
+import com.cumulocity.rest.representation.devicebootstrap.DeviceCredentialsRepresentation;
+import com.cumulocity.sdk.client.SDKException;
+import com.cumulocity.sdk.client.devicecontrol.DeviceCredentialsApi;
+import com.google.common.eventbus.Subscribe;
+import com.sun.jersey.api.client.ClientResponse.Status;
+
+public class DeviceBootstrapProcessor implements TrackerAgentEventListener {
+
+    private static final int POOL_SIZE = 2;
+    private static final int POLL_CREDENTIALS_TIMEOUT = 60;
+    private static final int POLL_CREDENTIALS_INTERVAL = 5;
+
+    protected static Logger logger = LoggerFactory.getLogger(DeviceBootstrapProcessor.class);
+
+    private final ExecutorService threadPoolExecutor;
+    private final Collection<String> duringBootstrap = new HashSet<String>();
+    private final Object lock = new Object();
+    private final TrackerAgent trackerAgent;
+    private final DeviceCredentialsApi deviceCredentialsApi;
+
+    public DeviceBootstrapProcessor(TrackerAgent trackerAgent) {
+        this.trackerAgent = trackerAgent;
+        this.threadPoolExecutor = Executors.newFixedThreadPool(POOL_SIZE);
+        this.deviceCredentialsApi = trackerAgent.getContext().getBootstrapPlatform().getDeviceCredentialsApi();
+    }
+
+    @Subscribe
+    public void listen(TrackerAgentEvents.NewDeviceEvent event) {
+        startBootstraping(event.getImei());
+    }
+
+    public void startBootstraping(String imei) {
+        synchronized (lock) {
+            if (duringBootstrap.contains(imei)) {
+                return;
+            }
+            duringBootstrap.add(imei);
+            DeviceBootstrapTask deviceBootstrapTask = new DeviceBootstrapTask(imei);
+            threadPoolExecutor.execute(deviceBootstrapTask);
+        }
+    }
+
+    private class DeviceBootstrapTask implements Runnable {
+
+        private final String imei;
+
+        public DeviceBootstrapTask(String imei) {
+            this.imei = imei;
+        }
+
+        @Override
+        public void run() {
+            try {
+                doRun();
+            } finally {
+                duringBootstrap.remove(imei);
+            }
+        }
+
+        private void doRun() {
+            try {
+                deviceCredentialsApi.hello(imei);
+            } catch (SDKException ex) {
+                boolean notFound = Status.NOT_FOUND.getStatusCode() == ex.getHttpStatus();
+                logger.warn("Hello from device for imei {} failed: {}.", imei, notFound ? "" : ex.getMessage());
+                return;
+            }
+            logger.info("Successfully sent hello from imei {}.", imei);
+            DeviceCredentialsRepresentation credentialsRepresentation = deviceCredentialsApi.pollCredentials(imei, POLL_CREDENTIALS_INTERVAL, POLL_CREDENTIALS_TIMEOUT);
+            if (credentialsRepresentation == null) {
+                logger.info("No credentials accessed for imei {}.", imei);
+            } else {
+                DeviceCredentials credentials = asCredentials(credentialsRepresentation);
+                logger.warn("Credentials for imei {} accessed: {}.", imei, credentials);
+                trackerAgent.sendEvent(new TrackerAgentEvents.NewDeviceRegisteredEvent(credentials));
+            }
+        }
+
+        private DeviceCredentials asCredentials(DeviceCredentialsRepresentation credentials) {
+            //@formatter:off
+            return new DeviceCredentials()
+                .setPassword(credentials.getPassword())
+                .setUser(credentials.getUsername())
+                .setTenantId(credentials.getTenantId())
+                .setImei(credentials.getId());
+            //@formatter:on
+        }
+
+    }
+}
