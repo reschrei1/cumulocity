fbe4730b79e31e2e390c6b6840013520c24fc69e
Georgi Huklev <georgi.huklev@cumulocity.com>
2014-06-30 13:03:32 +0200

    flow: Created branch 'feature/extend-linux-agent-tinkerforge-support'.

---
+++ linux-agent/tinkerforge-driver/src/main/java/c8y/tinkerforge/bricklet/DualRelayBricklet.java
@@ -0,0 +1,51 @@
+package c8y.tinkerforge.bricklet;
+
+import com.cumulocity.rest.representation.inventory.ManagedObjectRepresentation;
+import com.cumulocity.sdk.client.Platform;
+import com.tinkerforge.BrickletDualRelay;
+
+import c8y.lx.driver.Driver;
+import c8y.lx.driver.OperationExecutor;
+
+public class DualRelayBricklet implements Driver {
+
+	public DualRelayBricklet(String uid, BrickletDualRelay brickletDualRelay) {
+		// TODO Auto-generated constructor stub
+	}
+	
+    @Override
+    public void initialize() throws Exception {
+        // Nothing to do here.
+    }
+
+	@Override
+	public void initialize(Platform platform) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public OperationExecutor[] getSupportedOperations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initializeInventory(ManagedObjectRepresentation mo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void discoverChildren(ManagedObjectRepresentation mo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
