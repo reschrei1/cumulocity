505a745227924b1f92b30638a63a4b34f83c2294
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2014-07-23 09:17:19 +0200

    merge with feature/MTM-6948/tixi-agent3

--- tixi-agent/server/src/main/java/com/cumulocity/agent/server/repository/MeasurementRepository.java
+++ tixi-agent/server/src/main/java/com/cumulocity/agent/server/repository/MeasurementRepository.java
@@ -2,16 +2,20 @@ package com.cumulocity.agent.server.repository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.cumulocity.rest.representation.measurement.MeasurementRepresentation;
 import com.cumulocity.sdk.client.measurement.MeasurementApi;
 
+@Component
 public class MeasurementRepository {
 
     private static final Logger log = LoggerFactory.getLogger(MeasurementRepository.class);
 
     private final MeasurementApi measurementApi;
 
+    @Autowired
     public MeasurementRepository(MeasurementApi measurementApi) {
         this.measurementApi = measurementApi;
     }
