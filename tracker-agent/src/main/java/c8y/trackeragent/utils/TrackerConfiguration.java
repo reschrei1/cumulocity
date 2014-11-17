e69f969f67e74501d160a3e7e3b3e668d615a360
Lukasz Piasecki <Lukasz.Piasecki@impaqgroup.com>
2014-11-17 15:20:58 +0100

    Update dependency to next snapshot

---
+++ tracker-agent/src/main/java/c8y/trackeragent/utils/TrackerConfiguration.java
@@ -0,0 +1,72 @@
+package c8y.trackeragent.utils;
+
+public class TrackerConfiguration {
+
+    private String platformHost;
+    private int localPort;
+    private String bootstrapUser;
+    private String bootstrapPassword;
+    private String bootstrapTenant;
+    private int clientTimeout;
+
+    public String getPlatformHost() {
+        return platformHost;
+    }
+
+    public TrackerConfiguration setPlatformHost(String platformHost) {
+        this.platformHost = platformHost;
+        return this;
+    }
+
+    public int getLocalPort() {
+        return localPort;
+    }
+
+    public TrackerConfiguration setLocalPort(int localPort) {
+        this.localPort = localPort;
+        return this;
+    }
+
+    public String getBootstrapUser() {
+        return bootstrapUser;
+    }
+
+    public TrackerConfiguration setBootstrapUser(String bootstrapUser) {
+        this.bootstrapUser = bootstrapUser;
+        return this;
+    }
+
+    public String getBootstrapPassword() {
+        return bootstrapPassword;
+    }
+
+    public TrackerConfiguration setBootstrapPassword(String bootstrapPassword) {
+        this.bootstrapPassword = bootstrapPassword;
+        return this;
+    }
+
+    public TrackerConfiguration setBootstrapTenant(String bootstrapTenant) {
+        this.bootstrapTenant = bootstrapTenant;
+        return this;
+    }
+
+    public String getBootstrapTenant() {
+        return bootstrapTenant;
+    }
+    
+    public int getClientTimeout() {
+        return clientTimeout;
+    }
+
+    public TrackerConfiguration setClientTimeout(int clientTimeout) {
+        this.clientTimeout = clientTimeout;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("TrackerConfiguration [platformHost=%s, localPort=%s, bootstrapUser=%s, bootstrapPassword=%s, bootstrapTenant=%s, clientTimeout=%s]", platformHost, localPort,
+                bootstrapUser, bootstrapPassword, bootstrapTenant, clientTimeout);
+    }
+    
+}
