ad63d61beb73c794225470786d5280f6cea25d10
Dominik Bielicki <dominik.bielicki@impaqgroup.com>
2016-07-20 17:19:58 +0200

    fix problem of dateTime in esper getDate method

---
+++ tracker-agent/src/main/java/c8y/trackeragent/TrackerAgent.java
@@ -0,0 +1,52 @@
+package c8y.trackeragent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cumulocity.model.operation.OperationStatus;
+import com.cumulocity.rest.representation.operation.OperationRepresentation;
+import com.cumulocity.sdk.client.SDKException;
+
+import c8y.trackeragent.device.ManagedObjectCache;
+import c8y.trackeragent.device.TrackerDevice;
+import c8y.trackeragent.device.TrackerDeviceProvider;
+import c8y.trackeragent.devicebootstrap.DeviceCredentials;
+import c8y.trackeragent.devicebootstrap.DeviceCredentialsRepository;
+import c8y.trackeragent.exception.UnknownTenantException;
+import c8y.trackeragent.utils.TrackerPlatformProvider;
+
+@Component
+public class TrackerAgent {
+    
+	private final DeviceCredentialsRepository credentialsRepository;
+	private final TrackerPlatformProvider platformProvider;
+	private final TrackerDeviceProvider trackerDeviceProvider;
+	
+    @Autowired
+    public TrackerAgent(TrackerDeviceProvider trackerDeviceProvider, DeviceCredentialsRepository credentialsRepository,  TrackerPlatformProvider platformProvider) {
+		this.trackerDeviceProvider = trackerDeviceProvider;
+		this.credentialsRepository = credentialsRepository;
+		this.platformProvider = platformProvider;
+    }
+
+    public TrackerDevice getOrCreateTrackerDevice(String imei) throws SDKException {
+    	DeviceCredentials deviceCredentials = credentialsRepository.getDeviceCredentials(imei);
+    	return trackerDeviceProvider.getOrCreate(deviceCredentials.getTenant(), imei);
+    }
+    
+    public void finish(String deviceImei, OperationRepresentation operation) throws UnknownTenantException {
+        operation.setStatus(OperationStatus.SUCCESSFUL.toString());
+        getPlatform(deviceImei).getDeviceControlApi().update(operation);
+    }
+
+    public void fail(String deviceImei, OperationRepresentation operation, String text, SDKException ex) {
+        operation.setStatus(OperationStatus.FAILED.toString());
+        operation.setFailureReason(text + " " + ex.getMessage());
+        getPlatform(deviceImei).getDeviceControlApi().update(operation);
+    }
+        
+    private TrackerPlatform getPlatform(String imei) {
+    	DeviceCredentials deviceCredentials = credentialsRepository.getDeviceCredentials(imei);
+    	return platformProvider.getTenantPlatform(deviceCredentials.getTenant());
+    }
+}
---
+++ tracker-agent/src/main/java/c8y/trackeragent/TrackerPlatform.java
@@ -0,0 +1,189 @@
+package c8y.trackeragent;
+
+import java.util.concurrent.Callable;
+
+import com.cumulocity.sdk.client.Platform;
+import com.cumulocity.sdk.client.PlatformImpl;
+import com.cumulocity.sdk.client.PlatformParameters;
+import com.cumulocity.sdk.client.SDKException;
+import com.cumulocity.sdk.client.alarm.AlarmApi;
+import com.cumulocity.sdk.client.audit.AuditRecordApi;
+import com.cumulocity.sdk.client.cep.CepApi;
+import com.cumulocity.sdk.client.devicecontrol.DeviceControlApi;
+import com.cumulocity.sdk.client.devicecontrol.DeviceCredentialsApi;
+import com.cumulocity.sdk.client.event.EventApi;
+import com.cumulocity.sdk.client.identity.IdentityApi;
+import com.cumulocity.sdk.client.inventory.BinariesApi;
+import com.cumulocity.sdk.client.inventory.InventoryApi;
+import com.cumulocity.sdk.client.measurement.MeasurementApi;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import c8y.trackeragent.exception.SDKExceptions;
+
+@Deprecated //use context instead
+public class TrackerPlatform implements Platform {
+
+    private final Platform orig;
+    private Cache<Class<?>, Object> cache;
+
+    public TrackerPlatform(PlatformImpl orig) {
+        this.orig = orig;
+        this.cache = CacheBuilder.newBuilder().build();
+    }
+    
+    public InventoryApi getInventoryApi() throws SDKException {
+        return new CachedApiGetter<InventoryApi>(InventoryApi.class) {
+
+            @Override
+            public InventoryApi call() throws Exception {
+                return orig.getInventoryApi();
+            }
+
+        }.get();
+    }
+
+    public IdentityApi getIdentityApi() throws SDKException {
+        return new CachedApiGetter<IdentityApi>(IdentityApi.class) {
+
+            @Override
+            public IdentityApi call() throws Exception {
+                return orig.getIdentityApi();
+            }
+
+        }.get();
+    }
+
+    public MeasurementApi getMeasurementApi() throws SDKException {
+        return new CachedApiGetter<MeasurementApi>(MeasurementApi.class) {
+
+            @Override
+            public MeasurementApi call() throws Exception {
+                return orig.getMeasurementApi();
+            }
+
+        }.get();
+    }
+
+    public DeviceControlApi getDeviceControlApi() throws SDKException {
+        return new CachedApiGetter<DeviceControlApi>(DeviceControlApi.class) {
+
+            @Override
+            public DeviceControlApi call() throws Exception {
+                return orig.getDeviceControlApi();
+            }
+
+        }.get();
+    }
+
+    public AlarmApi getAlarmApi() throws SDKException {
+        return new CachedApiGetter<AlarmApi>(AlarmApi.class) {
+
+            @Override
+            public AlarmApi call() throws Exception {
+                return orig.getAlarmApi();
+            }
+
+        }.get();
+    }
+
+    public EventApi getEventApi() throws SDKException {
+        return new CachedApiGetter<EventApi>(EventApi.class) {
+
+            @Override
+            public EventApi call() throws Exception {
+                return orig.getEventApi();
+            }
+
+        }.get();
+    }
+
+    public AuditRecordApi getAuditRecordApi() throws SDKException {
+        return new CachedApiGetter<AuditRecordApi>(AuditRecordApi.class) {
+
+            @Override
+            public AuditRecordApi call() throws Exception {
+                return orig.getAuditRecordApi();
+            }
+
+        }.get();
+    }
+
+    public CepApi getCepApi() throws SDKException {
+        return new CachedApiGetter<CepApi>(CepApi.class) {
+
+            @Override
+            public CepApi call() throws Exception {
+                return orig.getCepApi();
+            }
+
+        }.get();
+    }
+    
+    @Override
+    public BinariesApi getBinariesApi() throws SDKException {
+        return new CachedApiGetter<BinariesApi>(BinariesApi.class) {
+
+            @Override
+            public BinariesApi call() throws Exception {
+                return orig.getBinariesApi();
+            }
+
+        }.get();
+    }
+
+    public DeviceCredentialsApi getDeviceCredentialsApi() throws SDKException {
+        return new CachedApiGetter<DeviceCredentialsApi>(DeviceCredentialsApi.class) {
+
+            @Override
+            public DeviceCredentialsApi call() throws Exception {
+                return orig.getDeviceCredentialsApi();
+            }
+
+        }.get();
+    }
+
+    public String getTenantId() {
+        return getPlatformParameters().getTenantId();
+    }
+
+    public String getHost() {
+        return getPlatformParameters().getHost();
+    }
+
+    public String getUser() {
+        return getPlatformParameters().getUser();
+    }
+
+    public String getPassword() {
+        return getPlatformParameters().getPassword();
+    }
+
+    public PlatformParameters getPlatformParameters() {
+        return (PlatformParameters) orig;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("TrackerPlatform [orig=%s, getTenantId()=%s, getHost()=%s, getUser()=%s]", orig, getTenantId(), getHost(), getUser());
+    }
+
+    abstract class CachedApiGetter<V> implements Callable<V> {
+
+        private final Class<V> cacheKey;
+
+        CachedApiGetter(Class<V> cacheKey) {
+            this.cacheKey = cacheKey;
+        }
+
+        @SuppressWarnings("unchecked")
+        V get() throws SDKException {
+            try {
+                return (V) cache.get(cacheKey, this);
+            } catch (Exception e) {
+                throw SDKExceptions.narrow(e, "Cant create api " + cacheKey.getSimpleName());
+            }
+        }
+    }
+
+}
