a7c02b260a15157c7357b99a10caae04c0515a6c
Ozge-Akin <ozge.akin@cumulocity.com>
2016-09-15 15:58:49 +0200

    hg flow, add branch `feature/MTM-13299/Queclink-devices`.

---
+++ tracker-agent/src/main/java/c8y/trackeragent/server/Servers.java
@@ -0,0 +1,38 @@
+package c8y.trackeragent.server;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import c8y.trackeragent.configuration.TrackerConfiguration;
+
+@Component
+public class Servers {
+    
+    private final TrackerConfiguration config;
+    private final ListableBeanFactory beanFactory;
+    private final ExecutorService executorService = newFixedThreadPool(2);
+    
+    @Autowired
+    public Servers(TrackerConfiguration config, ListableBeanFactory beanFactory) {
+        this.config = config;
+        this.beanFactory = beanFactory;
+    }
+
+    public void startAll() throws IOException {
+        startServer(config.getLocalPort1());
+        startServer(config.getLocalPort2());
+    }
+
+    private void startServer(int localPort) throws IOException {
+        TrackerServer server = beanFactory.getBean(TrackerServer.class);
+        server.start(localPort);
+        executorService.execute(server);
+    }        
+
+}
