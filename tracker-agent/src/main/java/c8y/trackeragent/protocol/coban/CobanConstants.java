4950eb03770140f20c5a51f979d6e3e281822aef
Ozge-Akin <ozge.akin@cumulocity.com>
2016-11-22 11:11:03 +0100

    hg flow, add branch `feature/MTM-13969/queclink-bugfix`.

---
+++ tracker-agent/src/main/java/c8y/trackeragent/protocol/coban/CobanConstants.java
@@ -0,0 +1,13 @@
+package c8y.trackeragent.protocol.coban;
+
+public class CobanConstants {
+
+    public static final Integer DEFAULT_LOCATION_REPORT_INTERVAL = 180;
+    
+    public static final String DEVICE_CONFIG_FRAGMENT = "coban_config";
+    public static final String DEVICE_CONFIG_KEY_LOCATION_REPORT_TIME_INTERVAL = "locationReportTimeInterval";
+    
+    public static final String GPS_OK = "F"; 
+    public static final String GPS_KO = "L"; 
+
+}
