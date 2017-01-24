5759adb26d4c7ae3169765f9ab91005f83143218
Arkadiusz Borek <arkadiusz.borek@cumulocity.com>
2017-01-24 22:34:59 +0100

    hg flow, add branch `release/r7.44.0`.

---
+++ tracker-agent/src/main/java/c8y/trackeragent/protocol/coban/message/CobanServerMessages.java
@@ -0,0 +1,42 @@
+package c8y.trackeragent.protocol.coban.message;
+
+import org.springframework.stereotype.Component;
+
+import c8y.trackeragent.protocol.TrackingProtocol;
+import c8y.trackeragent.utils.message.TrackerMessage;
+import c8y.trackeragent.utils.message.TrackerMessageFactory;
+
+@Component
+public class CobanServerMessages extends TrackerMessageFactory<TrackerMessage> {
+    
+    private static final String IMEI_PREFIX = "imei:";
+    
+    @Override
+    public TrackerMessage msg() {
+        return new TrackerMessage(TrackingProtocol.COBAN);
+    }
+
+    public TrackerMessage load() {
+        return msg().appendField("LOAD");
+    }
+    
+    public TrackerMessage on() {
+        return msg().appendField("ON");
+    }
+    
+    public TrackerMessage timeIntervalLocationRequest(String imei, String interval) {
+        return msg().appendField("**").appendField(imeiMsg(imei)).appendField("C").appendField(interval);
+    }
+    
+    public static String imeiMsg(String imei) {
+        return IMEI_PREFIX + imei;
+    }
+    
+    public static String extractImeiValue(String imeiPart) {
+        return imeiPart.replaceFirst(IMEI_PREFIX, "");
+    }
+
+
+    
+
+}
